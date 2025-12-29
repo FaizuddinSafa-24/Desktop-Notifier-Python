cb25ad1a118be43674c05324b8953e6b4c3e9f1e
FaizuddinSafa-24 <safarajuk19@gmail.com>
2025-12-29 12:11:21 +0600

    Implement try-except for notification errors
    Added exception handling to notification logic.

--- notifier.py
+++ notifier.py
@@ -4,7 +4,8 @@ import time
 title = 'Hi, Hello and Hi !'
 
 message = 'If u reading this ~I LOVE U TO THE MOON AND BACK !!'
-
+try:
+  # new thing. for exception to check bug, use try and except
 notification.notify(title = title,
                     message = message,
                     #creating icon for the notification
@@ -12,6 +13,8 @@ notification.notify(title = title,
                     #the notification stays for 15sec
                     timeout = 15,
                     toast = False)
+except Exception as e:
+    print(f"Oops! {e}")
 
 #sleep for 1 hr => 60*60 sec
 #notification repeats after every 1 hr
